a1cfe40df70f1f3a53ac1f4d15bd45621bcbe9fa
GChurchward <gus.griswold@hotmail.co.uk>
2022-08-07 11:07:29 -0700

    added extra functionality and debugged

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -4,13 +4,13 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { PostAuthor } from './PostAuthor'
+import { ReactionButtons } from './ReactionButtons'
+import { selectPostById } from './postsSlice'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector((state) =>
-    state.posts.find((post) => post.id === postId)
-  )
+  const post = useSelector((state) => selectPostById(state, postId))
 
   if (!post) {
     return (
@@ -28,6 +28,7 @@ export const SinglePostPage = ({ match }) => {
           <PostAuthor userId={post.user} />
         </div>
         <p className="post-content">{post.content}</p>
+        <ReactionButtons post={post} />
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
